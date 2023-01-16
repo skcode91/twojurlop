f319550fbe09f17def5d3cf2889a29131bd2d63f
Krzysztof Suski <krzysztof.suski@evorain.com>
2023-01-16 20:51:24 +0100

    axios client config

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,3 +1,5 @@
 import { createApiClient } from "src/common/configs/axiosClientConfig";
 
-export const apiClient = createApiClient("https://localhost:7120/api/");
+export const apiClient = createApiClient(
+  "https://api-twoj-urlop.azurewebsites.net/api/"
+);
